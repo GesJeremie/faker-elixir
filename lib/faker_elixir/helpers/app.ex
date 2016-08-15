bf30771d497d74a1f3e9811101974573e0e24b16
Jeremie ges <ges.jeremie@gmail.com>
2016-08-15 05:09:30 +0300

    comment Helpers.App

--- lib/faker_elixir/helpers/app.ex
+++ lib/faker_elixir/helpers/app.ex
@@ -1,12 +1,31 @@
 defmodule FakerElixir.Helpers.App do
 
-  @moduledocset false
+  @moduledoc false
 
+  @doc """
+  Generate a random number
+
+  ## Examples
+
+    iex > FakerElixir.Helpers.App.rand
+    0.34560389559786353
+  """
   def rand do
     :random.seed(:erlang.now)
     :random.uniform
   end
 
+  @doc """
+  Fill a pattern with random number
+
+  ## Examples
+
+    iex > FakerElixir.Helpers.App.numerify("Apt. ###")
+    "Apt. 902"
+
+    iex > FakerElixir.Helpers.App.numerify("06.##.##.##.##")
+    "06.67.18.21.27"
+  """
   def numerify(pattern) do
     pattern
       |> String.split("")
@@ -21,12 +40,53 @@ defmodule FakerElixir.Helpers.App do
     value
   end
 
+  @doc """
+  Pick randomly a value in an enumerable
+
+  ## Eamples
+
+    iex > FakerElixir.Helpers.App.pick(["paris", "athens", "london"])
+    "london"
+
+    iex > FakerElixir.Helpers.App.pick(0..100)
+    51
+  """
   def pick(enumerable) do
     enumerable
       |> Enum.shuffle
       |> Enum.at(0)
   end
 
+  @doc """
+  FakerElixir needs to manage the concept of "locales", `fetch/1` is useful
+  to retrieve the data wanted for the current locale. If the locale targeted
+  doesn't provide the data wanted, we just fallback to the default locale (en)
+
+  ## Examples
+
+    iex >  FakerElixir.Helpers.App.fetch(:cities)
+    ["New York City", "Los Angeles", "San Francisco", "Washington D.C", "Chicago",
+    "Boston", "Seattle", "Houston", "Detroit", "New Orleans", "Baltimore",
+    "Austin", "San Diego", "Portland", "Philadelphia", "Denver", "Minneapolis",
+    "San Jose", "Oakland", "Dallas", "Kansas City", "Miami", "Nashville",
+    "Atlanta", "Albuquerque", "Charlotte", "Memphis", "Milwaukee", "Raleigh",
+    "Cleveland", "Columbus", "El Paso", "Phoenix", "Tucson", "Las Vegas",
+    "Louisville", "Fort Worth", "Wichita", "Tulsa", "Jacksonville"]
+
+    iex > FakerElixir.set_locale(:fr)
+    :ok
+    iex > FakerElixir.Helpers.App.fetch(:cities)
+    ["Paris", "Marseille", "Lyon", "Toulouse", "Nice", "Nantes", "Strasbourg",
+     "Montpellier", "Bordeaux", "Lille", "Rennes", "Reims", "Le Havre",
+     "Saint-Étienne", "Toulon", "Grenoble", "Dijon", "Angers", "Saint-Denis",
+     "Villeurbanne", "Le Mans", "Aix-en-Provence", "Brest", "Nîmes", "Limoges",
+     "Clermont-Ferrand", "Tours", "Amiens", "Metz", "Perpignan", "Besançon",
+     "Orléans", "Boulogne-Billancourt", "Mulhouse", "Rouen", "Caen", "Nancy",
+     "Saint-Denis", "Saint-Paul", "Montreuil", "Argenteuil", "Roubaix",
+     "Dunkerque14", "Tourcoing", "Nanterre", "Avignon", "Créteil", "Poitiers",
+     "Fort-de-France", "Courbevoie", ...]
+
+  """
   def fetch(method) do
 
     # Fetch the current locale
