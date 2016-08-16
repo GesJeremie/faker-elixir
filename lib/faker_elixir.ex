962707a05034236592a50a4456d977927ee1c666
Jeremie ges <ges.jeremie@gmail.com>
2016-08-16 04:28:39 +0300

    test elixir base

--- lib/faker_elixir.ex
+++ lib/faker_elixir.ex
@@ -14,14 +14,35 @@ defmodule FakerElixir do
     Supervisor.start_link(children, opts)
   end
 
-  def get_locale() do
-    FakerElixir.Helpers.Store.get(:locale)
-  end
+  @doc """
+  You can set your favorite locale at the run time !
 
+  Note: Right now you can only pick between :fr and :en
+
+  ## Examples
+
+    iex> FakerElixir.set_locale(:fr)
+    :ok
+
+    iex> FakerElixir.set_locale(:en)
+    :ok
+
+    iex> FakerElixir.set_locale(:es)
+    :error
+  """
   def set_locale(locale) do
     if allowed_locale?(locale) do
       FakerElixir.Helpers.Store.set(:locale, locale)
+    else
+      :error
     end
+  end
+
+  @doc """
+  Return the current locale set
+  """
+  def get_locale() do
+    FakerElixir.Helpers.Store.get(:locale)
   end
 
   defp allowed_locale?(locale) do
--- test/faker_elixir_test.exs
+++ test/faker_elixir_test.exs
@@ -1,8 +1,17 @@
 defmodule FakerElixirTest do
   use ExUnit.Case
+
   doctest FakerElixir
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "get_locale/0" do
+    FakerElixir.set_locale(:en)
+    assert FakerElixir.get_locale == :en
+
+    FakerElixir.set_locale(:fr)
+    assert FakerElixir.get_locale == :fr
+
+    FakerElixir.set_locale(:es)
+    assert FakerElixir.get_locale == :fr
   end
+
 end
