dd6c6a8d34e807aace403fa4c66ca6efecaaa225
Jeremie ges <ges.jeremie@gmail.com>
2016-08-17 00:45:52 +0300

    Fix comments domain Helper

--- lib/faker_elixir/helper.ex
+++ lib/faker_elixir/helper.ex
@@ -10,69 +10,79 @@ defmodule FakerElixir.Helper do
 
   ## Examples
 
-    iex > FakerElixir.Helper.pick(["paris", "athens", "london"])
-    "london"
+  ```
+  iex> FakerElixir.Helper.pick(["paris", "athens", "london"])
+  "london"
+  ```
 
-    iex > FakerElixir.Helper.pick(0..100)
-    51
+  ```
+  iex> FakerElixir.Helper.pick(0..100)
+  51
+  ```
   """
   def pick(enumerable), do: App.pick(enumerable)
 
   @doc """
-  Will iterate through the enumerable as a constant cycle
-  Really useful when you want to seed your database with a pre-defined cycle
+  Will iterate through the enumerable as a constant cycle. Really useful when you want to seed your database with a pre-defined cycle
 
-  Warning: The id (first param given) should be unique for each different cycle
+  ```Warning:``` The id (first param given) should be unique for each different cycle
 
   ## Examples
 
-    ### Basic
+  #### Basic
 
-    iex(1)> FakerElixir.Helper.cycle(:zombies, ["Peter", "Audrey", "Laurent", "Frank"])
-    "Peter"
-    iex(2)> FakerElixir.Helper.cycle(:zombies, ["Peter", "Audrey", "Laurent", "Frank"])
-    "Audrey"
-    iex(3)> FakerElixir.Helper.cycle(:zombies, ["Peter", "Audrey", "Laurent", "Frank"])
-    "Laurent"
-    iex(4)> FakerElixir.Helper.cycle(:zombies, ["Peter", "Audrey", "Laurent", "Frank"])
-    "Frank"
-    iex(5)> FakerElixir.Helper.cycle(:zombies, ["Peter", "Audrey", "Laurent", "Frank"])
-    "Peter"
-    iex(6)> FakerElixir.Helper.cycle(:zombies, ["Peter", "Audrey", "Laurent", "Frank"])
-    "Audrey"
-    iex(7)> FakerElixir.Helper.cycle(:zombies, ["Peter", "Audrey", "Laurent", "Frank"])
-    "Laurent"
-    iex(8)> FakerElixir.Helper.cycle(:zombies, ["Peter", "Audrey", "Laurent", "Frank"])
-    "Frank"
+  ```
+  iex(1)> FakerElixir.Helper.cycle(:zombies, ["Peter", "Audrey", "Laurent", "Frank"])
+  "Peter"
+  iex(2)> FakerElixir.Helper.cycle(:zombies, ["Peter", "Audrey", "Laurent", "Frank"])
+  "Audrey"
+  iex(3)> FakerElixir.Helper.cycle(:zombies, ["Peter", "Audrey", "Laurent", "Frank"])
+  "Laurent"
+  iex(4)> FakerElixir.Helper.cycle(:zombies, ["Peter", "Audrey", "Laurent", "Frank"])
+  "Frank"
+  iex(5)> FakerElixir.Helper.cycle(:zombies, ["Peter", "Audrey", "Laurent", "Frank"])
+  "Peter"
+  iex(6)> FakerElixir.Helper.cycle(:zombies, ["Peter", "Audrey", "Laurent", "Frank"])
+  "Audrey"
+  iex(7)> FakerElixir.Helper.cycle(:zombies, ["Peter", "Audrey", "Laurent", "Frank"])
+  "Laurent"
+  iex(8)> FakerElixir.Helper.cycle(:zombies, ["Peter", "Audrey", "Laurent", "Frank"])
+  "Frank"
 
-    ... and so on.
+  # ... and so on.
+  ```
 
-    ### Seed example
+  #### Seed example
 
-    defmodule Seed do
+  ```
+  defmodule Seed do
 
-      alias FakerElixir.Helper
+    alias FakerElixir.Helper
 
-      def make do
-        warriors = Stream.repeatedly(fn() -> fixture(:warrior) end)
-          |> Enum.take(5)
-      end
-
-
-      defp fixture(:warrior) do
-        %{
-          name: Helper.cycle(:name, ["anubis", "zeus", "behamut"]),
-          lvl: Helper.cycle(:lvl, [10, 25, 90])
-        }
-      end
-
+    def make do
+      warriors = Stream.repeatedly(fn() -> fixture(:warrior) end)
+        |> Enum.take(5)
     end
 
-    iex> Seed.make
-    [%{lvl: 10, name: "anubis"}, %{lvl: 25, name: "zeus"},
-    %{lvl: 90, name: "behamut"}, %{lvl: 10, name: "anubis"},
-    %{lvl: 25, name: "zeus"}]
 
+    defp fixture(:warrior) do
+      %{
+        name: Helper.cycle(:name, ["anubis", "zeus", "behamut"]),
+        lvl: Helper.cycle(:lvl, [10, 25, 90])
+      }
+    end
+
+  end
+
+  iex> Seed.make
+  [
+    %{lvl: 10, name: "anubis"},
+    %{lvl: 25, name: "zeus"},
+    %{lvl: 90, name: "behamut"},
+    %{lvl: 10, name: "anubis"},
+    %{lvl: 25, name: "zeus"}
+  ]
+  ```
   """
   def cycle(id, enumerable) do
 
