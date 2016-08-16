852841b3e8e7f5b6b39616cc74f021ad5e7996ee
Jeremie ges <ges.jeremie@gmail.com>
2016-08-16 20:14:56 +0300

    Tests App domain

---
+++ test/faker_elixir/app_test.exs
@@ -0,0 +1,19 @@
+defmodule FakerElixir.AppTest do
+
+  use ExUnit.Case
+  alias FakerElixir.App
+
+
+  test "name/0" do
+    assert is_binary(App.name)
+  end
+
+  test "version/0" do
+    assert is_binary(App.version)
+  end
+
+  test "author/0" do
+    assert is_binary(App.author)
+  end
+
+end
