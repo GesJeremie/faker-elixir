d7861e460033d93585608f8cb692e2ebde226177
Jeremie ges <ges.jeremie@gmail.com>
2016-08-20 03:23:43 +0300

    Internet password method in progress

--- lib/faker_elixir/internet.ex
+++ lib/faker_elixir/internet.ex
@@ -161,4 +161,109 @@ defmodule FakerElixir.Internet do
     "www.#{first_name}-#{last_name}.#{extension}"
   end
 
+
+  def password(:weak) do
+    0..1
+      |> pick
+      |> do_password_weak
+  end
+
+  def password(:normal) do
+    0..1
+      |> pick
+      |> do_password_normal
+  end
+
+  def password(:strong) do
+
+  end
+
+
+  ##
+  # What's a weak password ?
+  #
+  # - Default passwords such as: password, default, admin, guest, etc.
+  # - Common sequences from a keyboard row:  qwerty, 12345, asdfgh, fred, etc
+  # - Firstname
+  ##
+  defp do_password_weak(0) do
+    :weak_passwords
+      |> fetch
+      |> pick
+  end
+
+  defp do_password_weak(1) do
+    :first_names
+      |> fetch
+      |> pick
+      |> keep_strict_alpha_numeric
+      |> String.downcase
+  end
+
+  ##
+  # What's a normal password ?
+  #
+  # - First name with numbers
+  # - Full name
+  #
+  # Complexifiers:
+  # - Upcase first char
+  ##
+  defp do_password_normal(0) do
+    number_patterns = ["#", "##", "###"]
+
+    first_name = (:first_names |> fetch |> pick |> normalize_name)
+    number = (number_patterns |> pick |> numerify)
+
+    password = "#{first_name}#{number}"
+
+    if should_complexify? do
+      normal_complexifier(password)
+    else
+      password
+    end
+
+  end
+
+  defp do_password_normal(1) do
+    first_name = :first_names |> fetch |> pick |> normalize_name
+    last_name = :last_names |> fetch |> pick |> normalize_name
+
+    password = "#{first_name}#{last_name}"
+
+    if should_complexify? do
+      normal_complexifier(password)
+    else
+      password
+    end
+  end
+
+  defp normal_complexifier(password) do
+    (password |> String.capitalize)
+  end
+
+  ##
+  # What's a strong password ?
+  #
+  # - Something which just doesn't mean shit
+  #
+  # Complexifiers:
+  # - Length more than 12 chars
+  # - Special chars
+  # - Some upcase
+  ##
+  defp do_password_strong do
+
+  end
+
+  defp should_complexify? do
+    ([true, false] |> pick)
+  end
+
+  defp normalize_name(name) do
+    name
+      |> keep_strict_alpha_numeric
+      |> String.downcase
+  end
+
 end
