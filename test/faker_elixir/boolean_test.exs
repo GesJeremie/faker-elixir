b8afaa14806107385456a2590083e0d5eb4d629e
Jeremie ges <ges.jeremie@gmail.com>
2016-08-21 19:33:00 +0300

    Refactor tests Boolean

--- test/faker_elixir/boolean_test.exs
+++ test/faker_elixir/boolean_test.exs
@@ -3,27 +3,38 @@ defmodule FakerElixir.BooleanTest do
   use ExUnit.Case
   alias FakerElixir.Boolean
 
-  test "boolean/0" do
-    assert is_boolean(Boolean.boolean)
+  describe "boolean/0" do
+
+    test "return boolean" do
+        assert is_boolean(Boolean.boolean)
+    end
+
   end
 
-  test "boolean/1" do
-    assert is_boolean(Boolean.boolean(0))
-    assert is_boolean(Boolean.boolean(1))
-    assert is_boolean(Boolean.boolean(0.4))
+  describe "boolean/1" do
+
+    test "return boolean" do
+      assert is_boolean(Boolean.boolean(0.2))
+    end
+
+    test "never return false for ratio 1" do
+      cases = Stream.repeatedly(fn() -> Boolean.boolean(1) end)
+      results =
+        cases
+        |> Enum.take(1000)
+
+      assert Enum.member?(results, false) == false
+    end
+
+    test "never return true for ratio 0" do
+      cases = Stream.repeatedly(fn() -> Boolean.boolean(0) end)
+      results =
+        cases
+        |> Enum.take(1000)
+
+      assert Enum.member?(results, true) == false
+    end
+
   end
 
-  test "boolean/1 never return false for ratio 1" do
-    results = Stream.repeatedly(fn() -> Boolean.boolean(1) end)
-      |> Enum.take(500)
-
-    assert Enum.member?(results, false) == false
-  end
-
-  test "boolean/1 never return true for ratio 0" do
-    results = Stream.repeatedly(fn() -> Boolean.boolean(0) end)
-      |> Enum.take(500)
-
-    assert Enum.member?(results, true) == false
-  end
 end
