77a94cb35b07ca049657ca41a2007291950e1965
Jeremie ges <ges.jeremie@gmail.com>
2016-08-22 01:17:37 +0300

    Bank domain in progress

---
+++ lib/faker_elixir/bank.ex
@@ -0,0 +1,39 @@
+defmodule FakerElixir.Bank do
+
+  def credit_card_number do
+
+  end
+
+  def credit_card_type do
+
+  end
+
+  def credit_card_cvv do
+
+  end
+
+  def credit_card_expiration_date do
+
+  end
+
+  def credit_card_expiration_date(:valid) do
+
+  end
+
+  def credit_card_expiration_date(:invalid) do
+
+  end
+
+  def iban do
+
+  end
+
+  def swift do
+
+  end
+
+  def name do
+
+  end
+
+end
