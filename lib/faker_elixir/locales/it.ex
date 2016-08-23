0be4f5c881473e19402674bb1c01c22305fa9ec7
Jeremie ges <ges.jeremie@gmail.com>
2016-08-24 00:27:34 +0300

    Add locale it

---
+++ lib/faker_elixir/locales/it.ex
@@ -0,0 +1,11 @@
+defmodule FakerElixir.Locales.It do
+
+  @moduledoc false
+
+  @bank_names ["UniCredit", "Intesa Sanpaolo", "Banca d'Italia", "Cassa Depositi e Prestiti", "Banca Monte dei Paschi di Siena", "Banca Popolare di Milano", "UBI Banca", "Banca Nazionale del Lavoro", "Mediobanca", "Banca Popolare dell'Emilia Romagna", "Cariparma", "ICCREA Banca", "Banca Mediolanum", "Banca Carige", "Crediop", "Banca Popolare di Sondrio", "Credito Emiliano", "Credito Valtellinese", "Banca Sella Group", "Banca Popolare di Bari", "Banco di Desio e della Brianza", "Unipol Banca", "Cassa di Risparmio di Asti"]
+  @colors ["rosso", "verde", "blu", "giallo", "viola", "menta verde", "bianca", "nero", "arancia", "rosa", "grigio", "marrone", "turchese", "cielo blu", "oro", "argento"]
+
+  def bank_names, do: @bank_names
+  def colors, do: @colors
+
+end
