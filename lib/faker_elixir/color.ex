4839a0834c771d55104e87faeb11a9874568e666
Jeremie ges <ges.jeremie@gmail.com>
2016-08-23 22:23:02 +0300

    HEX documentation for domain Color

--- lib/faker_elixir/color.ex
+++ lib/faker_elixir/color.ex
@@ -1,11 +1,34 @@
 defmodule FakerElixir.Color do
+  @moduledoc """
+  Generate fake date for the domain Color
+  """
 
   import FakerElixir.Helpers.App
 
+  @doc """
+  Return name
+
+  ## Examples
+
+  ```
+  iex> FakerElixir.Color.name
+  "white"
+  ```
+  """
   def name do
     :colors |> fetch |> pick
   end
 
+  @doc """
+  Return hex
+
+  ## Examples
+
+  ```
+  iex> FakerElixir.Color.hex
+  "#1671B0"
+  ```
+  """
   def hex do
     hex = :io_lib.format("~2.16.0B~2.16.0B~2.16.0B", make_rgb())
     |> :erlang.iolist_to_binary
@@ -13,11 +36,31 @@ defmodule FakerElixir.Color do
     "##{hex}"
   end
 
+  @doc """
+  Return a list with rgb values
+
+  ## Examples
+
+  ```
+  iex> FakerElixir.Color.make_rgb
+  [65, 137, 5]
+  ```
+  """
   def make_rgb do
     stream = Stream.repeatedly(&rgb_color/0)
     stream |> Enum.take(3)
   end
 
+  @doc """
+  Return rgb
+
+  ## Examples
+
+  ```
+  iex> FakerElixir.Color.rgb
+  "rgb(152, 228, 47)"
+  ```
+  """
   def rgb do
     [color1, color2, color3] = make_rgb()
 
@@ -28,10 +71,30 @@ defmodule FakerElixir.Color do
     0..255 |> pick
   end
 
+  @doc """
+  Return a list with hsl values
+
+  ## Examples
+
+  ```
+  iex> FakerElixir.Color.make_hsl
+  [236, "13%", "77%"]
+  ```
+  """
   def make_hsl do
     [pick(0..360), "#{pick(0..100)}%", "#{pick(0..100)}%"]
   end
 
+  @doc """
+  Return hsl
+
+  ## Examples
+
+  ```
+  iex> FakerElixir.Color.hsl
+  "hsl(130, 40%, 41%)"
+  ```
+  """
   def hsl do
     [color1, color2, color3] = make_hsl()
 
