85fb33aaa27992d45d794086ae70e43a8ff6fab6
Jeremie ges <ges.jeremie@gmail.com>
2016-08-23 22:57:29 +0300

    Add locale es

---
+++ lib/faker_elixir/locales/es.ex
@@ -0,0 +1,11 @@
+defmodule FakerElixir.Locales.Es do
+
+  @moduledoc false
+
+  @bank_names ["Banco Santander", "BBVA", "CaixaBank", "Bankia", "Banco de Sabadell", "Banco Popular Espanol", "Unicaja Banco", "Ibercaja Banco", "KutxaBank", "Bankinter", "Banco Mare Nostrum", "Liberbank"]
+  @colors ["rojo", "verde", "azul", "amarillo", "violeta", "menta verde", "blanco", "negro", "naranja", "rosado", "gris", "marrón", "turquesa", "magenta", "amarillo limón", "plata"]
+
+  def bank_names, do: @bank_names
+  def colors, do: @colors
+
+end
