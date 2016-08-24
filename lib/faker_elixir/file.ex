35de3aea70044e31286b1fd5ec31f43a96b72342
Jeremie ges <ges.jeremie@gmail.com>
2016-08-24 18:20:01 +0300

    File domain in progress

---
+++ lib/faker_elixir/file.ex
@@ -0,0 +1,29 @@
+defmodule FakerElixir.File do
+
+  def extension do
+
+  end
+
+  def extension(type) do
+
+  end
+
+  def name do
+
+  end
+
+  def name(type) do
+
+  end
+
+  def mime do
+
+  end
+
+  def mime(kind) do
+    
+  end
+
+
+
+end
