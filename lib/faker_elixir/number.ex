dea2f8d99fc6ee02c2537c6ced509af1863debbe
Jeremie ges <ges.jeremie@gmail.com>
2016-08-26 03:52:48 +0300

    Add domain Number

---
+++ lib/faker_elixir/number.ex
@@ -0,0 +1,145 @@
+defmodule FakerElixir.Number do
+
+  import FakerElixir.Helpers.App
+
+  @doc """
+  Return a digit
+
+  ## Examples
+
+  ```
+  iex> FakerElixir.Number.digit
+  3
+  ```
+  """
+  def digit do
+    0..9 |> pick
+  end
+
+  @doc """
+  Return random digits
+
+  ## Examples
+
+  ```
+  iex> FakerElixir.Number.digits
+  452
+  ```
+  """
+  def digits do
+    1..6 |> pick |> digits
+  end
+
+  @doc """
+  Return ```x``` digits
+
+  ## Examples
+
+  ```
+  iex> FakerElixir.Number.digits(4)
+  1234523456
+  ```
+  """
+  def digits(length) do
+    {digits, _} =
+      Stream.repeatedly(&digit/0)
+      |> Enum.take(length)
+      |> Enum.join
+      |> Integer.parse
+
+    digits
+  end
+
+
+  @doc """
+  Return a number between the range given
+
+  ## Examples
+
+  ```
+  iex> FakerElixir.Number.between(15..150)
+  130
+  ```
+  """
+  def between(range) when is_map(range) do
+    range |> pick
+  end
+
+  @doc """
+  Return a number between the min and max given
+
+  ## Examples
+
+  ```
+  iex> FakerElixir.Number.between(15, 150)
+  18
+
+  iex> FakerElixir.Number.between(11.22, 13.88)
+  11.24
+
+  iex> FakerElixir.Number.between(12, 12.35)
+  12.11
+  ```
+  """
+  def between(min, max) when is_integer(min) and is_integer(max) do
+    min..max |> pick
+  end
+
+  def between(min, max) do
+
+    # Convert everything to float
+    min = min * 1.0
+    max = max * 1.0
+
+    result = rand() * (max - min) + min
+
+    result |> Float.round(2)
+  end
+
+  @doc """
+  Return a random decimal number
+
+  ## Examples
+
+  ```
+  iex> FakerElixir.Number.decimal
+  "70.8"
+  ```
+  """
+  def decimal do
+    decimal((1..2 |> pick), (1..2 |> pick))
+  end
+
+  @doc """
+  Return a random decimal number with ```x``` left digits
+
+  ## Examples
+
+  ```
+  iex> FakerElixir.Number.decimal(3)
+  "918.43"
+  ```
+  """
+  def decimal(left_digits) do
+    decimal(left_digits, (1..3 |> pick))
+  end
+
+  @doc """
+  Return a random decimal number with ```x``` left digits and ```x```
+  right digits
+
+  ## Examples
+
+  ```
+  iex> FakerElixir.Number.decimal(1, 3)
+  "2.298"
+  ```
+  """
+  def decimal(left_digits, right_digits) do
+    left = digits(left_digits)
+    right = digits(right_digits)
+
+    "#{left}.#{right}"
+  end
+
+end
