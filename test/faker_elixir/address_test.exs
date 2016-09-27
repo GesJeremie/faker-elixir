0d0e5aae236f6e11ee7b45c295b5e6e82c8a1f1d
Jeremie ges <ges.jeremie@gmail.com>
2016-09-27 06:37:15 +0300

    Add alias module Address test to reduce the amount of code

--- test/faker_elixir/address_test.exs
+++ test/faker_elixir/address_test.exs
@@ -1,11 +1,12 @@
 defmodule FakerElixir.AddressTest do
 
   use ExUnit.Case, async: true
+  alias FakerElixir.Address
 
   describe "city/0" do
 
     test "return binary" do
-      assert is_binary(FakerElixir.Address.city)
+      assert is_binary(Address.city)
     end
 
   end
@@ -13,7 +14,7 @@ defmodule FakerElixir.AddressTest do
   describe "make_country/0" do
 
     test "return map" do
-      country = FakerElixir.Address.make_country
+      country = Address.make_country
       assert is_map(country)
       assert Map.has_key?(country, :name)
       assert Map.has_key?(country, :code)
@@ -24,7 +25,7 @@ defmodule FakerElixir.AddressTest do
   describe "country/0" do
 
     test "return binary" do
-      assert is_binary(FakerElixir.Address.country)
+      assert is_binary(Address.country)
     end
 
   end
@@ -32,7 +33,7 @@ defmodule FakerElixir.AddressTest do
   describe "country_code/0" do
 
     test "return binary" do
-      assert is_binary(FakerElixir.Address.country_code)
+      assert is_binary(Address.country_code)
     end
 
   end
@@ -40,7 +41,7 @@ defmodule FakerElixir.AddressTest do
   describe "street_suffix/0" do
 
     test "return binary" do
-      assert is_binary(FakerElixir.Address.street_suffix)
+      assert is_binary(Address.street_suffix)
     end
 
   end
@@ -48,7 +49,7 @@ defmodule FakerElixir.AddressTest do
   describe "street_name/0" do
 
     test "return binary" do
-      assert is_binary(FakerElixir.Address.street_name)
+      assert is_binary(Address.street_name)
     end
 
   end
@@ -56,7 +57,7 @@ defmodule FakerElixir.AddressTest do
   describe "street_address/0" do
 
     test "return binary" do
-      assert is_binary(FakerElixir.Address.street_address)
+      assert is_binary(Address.street_address)
     end
 
   end
@@ -65,7 +66,7 @@ defmodule FakerElixir.AddressTest do
   describe "building_number/0" do
 
     test "return binary" do
-      assert is_binary(FakerElixir.Address.building_number)
+      assert is_binary(Address.building_number)
     end
 
   end
@@ -73,7 +74,7 @@ defmodule FakerElixir.AddressTest do
   describe "zip_code/0" do
 
     test "return binary" do
-      assert is_binary(FakerElixir.Address.zip_code)
+      assert is_binary(Address.zip_code)
     end
 
   end
@@ -81,7 +82,7 @@ defmodule FakerElixir.AddressTest do
   describe "time_zone/0" do
 
     test "return binary" do
-      assert is_binary(FakerElixir.Address.time_zone)
+      assert is_binary(Address.time_zone)
     end
 
   end
@@ -89,7 +90,7 @@ defmodule FakerElixir.AddressTest do
   describe "state/0" do
 
     test "return binary" do
-      assert is_binary(FakerElixir.Address.state)
+      assert is_binary(Address.state)
     end
 
   end
@@ -97,7 +98,7 @@ defmodule FakerElixir.AddressTest do
   describe "state_code/0" do
 
     test "return binary" do
-      assert is_binary(FakerElixir.Address.state_code)
+      assert is_binary(Address.state_code)
     end
 
   end
@@ -105,11 +106,11 @@ defmodule FakerElixir.AddressTest do
   describe "latitude/0" do
 
     test "return float" do
-      assert is_float(FakerElixir.Address.latitude)
+      assert is_float(Address.latitude)
     end
 
     test "correct latitude" do
-      assert FakerElixir.Address.latitude |> between?(-90, 90)
+      assert Address.latitude |> between?(-90, 90)
     end
 
   end
@@ -117,11 +118,11 @@ defmodule FakerElixir.AddressTest do
   describe "longitude/0" do
 
     test "return float" do
-      assert is_float(FakerElixir.Address.longitude)
+      assert is_float(Address.longitude)
     end
 
     test "correct longitude" do
-      assert FakerElixir.Address.longitude |> between?(-180, 180)
+      assert Address.longitude |> between?(-180, 180)
     end
 
   end
