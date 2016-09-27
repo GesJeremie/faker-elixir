dba19487af55887425ed427cef9b25b29988c527
Jeremie ges <ges.jeremie@gmail.com>
2016-09-27 06:32:12 +0300

    Comment generator Currency module

--- lib/faker_elixir/currency.ex
+++ lib/faker_elixir/currency.ex
@@ -6,18 +6,58 @@ defmodule FakerElixir.Currency do
 
   import FakerElixir.Helpers.App
   
+  @doc """
+  Return currency code
+
+  ## Example
+
+  ```
+  iex> FakerElixir.Currency.code
+  "ZMK"
+  ```
+  """
   def code do    
    fetch_currency.code
   end
 
+  @doc """
+  Return currency symbol
+
+  ## Example
+
+  ```
+  iex> FakerElixir.Currency.symbol
+  "€"
+  ```
+  """
   def symbol do
     fetch_currency.symbol
   end
 
+  @doc """
+  Return currency name
+
+  ## Example
+
+  ```
+  iex> FakerElixir.Currency.name
+  "East Caribbean Dollar"
+  ```
+  """
   def name do
     fetch_currency.name
   end
 
+  @doc """
+  Return currency struct with different keys
+
+  ## Example
+
+  ```
+  iex> FakerElixir.Currency.make
+  %{code: "UAH", country: "Ukraine", name: "Ukrainian Hryvnia", symbol: "₴"}
+  ```
+  """
   def make do
     fetch_currency
   end
