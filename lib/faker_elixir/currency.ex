2a3432caf507b55d30fe04e342d84ac8177db056
Jeremie ges <ges.jeremie@gmail.com>
2016-09-27 06:23:20 +0300

    Name methods module currency

---
+++ lib/faker_elixir/currency.ex
@@ -0,0 +1,25 @@
+defmodule FakerElixir.Currency do
+
+  @moduledoc """
+  Generate fake data for the domain currency
+  """
+
+  alias FakerElixir.Helpers.App
+  
+  def code do
+
+  end
+
+  def symbol do
+    
+  end
+
+  def name do
+    
+  end
+
+  def make do
+    
+  end
+
+end
