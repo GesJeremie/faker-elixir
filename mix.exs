f984110316bdd948ba50c3ae6959f77bf4580f7b
Ges Jérémie <ges.jeremie@gmail.com>
2017-06-07 19:28:14 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule FakerElixir.Mixfile do
 
   def project do
     [app: :faker_elixir_octopus,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
